f1765c7ccca9a79248fd8b3023221f4e411a9de2
HVish <vishnusingh1729@gmail.com>
2016-02-11 03:47:00 +0530

    updated commitee page

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
     $('.loader').remove();
     $('#mainbody').removeClass('hide');
     //initialization
-    $('.scrollspy').scrollSpy();
+    //$('.scrollspy').scrollSpy();
     $('.slider').slider({full_width: true, indicators: false});
     $(".button-collapse").sideNav();
     $('.collapsible').collapsible();
